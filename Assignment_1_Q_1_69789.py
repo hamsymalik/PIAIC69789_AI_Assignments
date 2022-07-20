992e494c21ad9fbef10986c818687d18946e9571
Hamza Ihtesham <hamzaimalik.995@gmail.com>
2022-07-20 22:50:14 +0500

    Assignment 1 - Question 1

---
+++ Assignment_1_Q_1_69789.py
@@ -0,0 +1,6 @@
+print ("\n\n\t\t\tPIAIC69789\n\t\t\tASSIGNMENT 1 - QUESTION 1\n\t\t\tCHECKING EVEN AND ODD NUMBERS")
+x = (input("\n\n\t\t\tENTER NUMBER: "))
+if (int(x)%2):
+    print ("\n\t\t\tThe entered number is odd.\n\n")
+else:
+    print("\n\t\t\tThe entered number is even.\n\n")
